27c7e11f32a2fa86193a6d08db8e4bbd5dbc5485
MontBru <monticelli.bryan@gmail.com>
2025-08-13 14:41:20 +0300

    created project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,65 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#347928"
+    },
+    secondary: {
+      main: "#C0EBA6"
+    }
+  },
+  typography: {
+    // Define general typography styles here
+    fontFamily: "Arial", // you can change to any font
+    h1: {
+      fontWeight: 600,
+      fontSize: '2.5rem',  // Default for larger screens
+      '@media (max-width:600px)': {
+        fontSize: '1.8rem', // Smaller for mobile
+      },
+    },
+    h2: {
+      fontWeight: 500,
+      fontSize: '2rem',   // Default for larger screens
+      '@media (max-width:600px)': {
+        fontSize: '1.5rem', // Smaller for mobile
+      },
+    },
+    body1: {
+      fontSize: '1.8rem',  // Default for larger screens
+      '@media (max-width:600px)': {
+        fontSize: '1.3rem', // Smaller for mobile
+      },
+    },
+    body2: {
+      fontSize: '1.3rem', // Default for larger screens
+      '@media (max-width:600px)': {
+        fontSize: '1rem', // Smaller for mobile
+      },
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
